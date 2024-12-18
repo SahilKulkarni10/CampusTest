f094d54759b86e5c1c6b750ee7aa81815739845e
sahil kulkarni <sahilkulkarni@Sahils-MacBook-Air-4.local>
2024-12-19 01:06:48 +0530

    latest new on

--- api/app.js
+++ api/app.js
@@ -1,6 +1,50 @@
+// import express from "express";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import authRoute from "./routes/auth.route.js";
+// import postRoute from "./routes/post.route.js";
+// import testRoute from "./routes/test.route.js";
+// import userRoute from "./routes/user.route.js";
+// import chatRoute from "./routes/chat.route.js";
+// import messageRoute from "./routes/message.route.js";
+
+// const app = express();
+
+// const allowedOrigins = [process.env.CLIENT_URL_LOCAL, process.env.CLIENT_URL_PROD];
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/posts", postRoute);
+// app.use("/api/test", testRoute);
+// app.use("/api/chats", chatRoute);
+// app.use("/api/messages", messageRoute);
+
+// app.listen(8800, () => {
+//   console.log("Server is running on 8800!");
+// });
+
+
+
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { Server } from "socket.io";
+import http from "http";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
@@ -9,7 +53,18 @@ import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 
 const app = express();
+const server = http.createServer(app); // Create an HTTP server
+const io = new Server(server, {
+  cors: {
+    origin: [
+      process.env.CLIENT_URL_LOCAL,
+      process.env.CLIENT_URL_PROD,
+    ],
+    credentials: true,
+  },
+});
 
+// CORS Middleware
 const allowedOrigins = [process.env.CLIENT_URL_LOCAL, process.env.CLIENT_URL_PROD];
 app.use(
   cors({
@@ -27,6 +82,7 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+// Routes
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
@@ -34,6 +90,51 @@ app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
-app.listen(8800, () => {
-  console.log("Server is running on 8800!");
+// Socket.IO Logic
+let onlineUsers = [];
+
+const addUser = (userId, socketId) => {
+  if (!onlineUsers.some((user) => user.userId === userId)) {
+    onlineUsers.push({ userId, socketId });
+  }
+};
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return onlineUsers.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  // Add new user
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id);
+    console.log("New user added:", onlineUsers);
+  });
+
+  // Send message
+  socket.on("sendMessage", ({ receiverId, data }) => {
+    const receiver = getUser(receiverId);
+    if (receiver) {
+      io.to(receiver.socketId).emit("getMessage", data);
+    } else {
+      console.log(`User ${receiverId} not found.`);
+    }
+  });
+
+  // Disconnect
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+    removeUser(socket.id);
+  });
+});
+
+// Start Server
+const PORT = process.env.PORT || 8800;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
 });
