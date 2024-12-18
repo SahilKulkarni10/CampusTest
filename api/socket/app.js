30ae251a23610557e6d1b5e04644d7acee9b9de2
sahil kulkarni <sahilkulkarni@Sahils-MacBook-Air-4.local>
2024-12-19 00:52:09 +0530

    latest new

--- api/socket/app.js
+++ api/socket/app.js
@@ -53,7 +53,7 @@ const io = new Server({
   cors: {
     origin: [
       "http://localhost:5173",
-      "",
+      "https://campus-test-six.vercel.app",
     ],
   },
 });
--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -15,7 +15,7 @@ const apiRequest = axios.create({
   baseURL:
     window.location.hostname === "localhost"
       ? "http://localhost:8800/api"
-      : "",
+      : "https://campus-test-zz76.vercel.app",
   withCredentials: true,
 });
 
